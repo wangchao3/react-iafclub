175f1a3cf22d7934a523a6dde32110f727c54118
wangchao <wangchao@iafclub.com>
2017-07-25 18:06:54 +0800

    我的

--- src/app/my/components/home.jsx
+++ src/app/my/components/home.jsx
@@ -8,6 +8,8 @@ import styles from '../styles/home'
 import Footer from '../../../components/footer'
 import Header from '../../common/components/header'
 import cx from 'classnames'
+import {Link} from 'react-router'
+import {getUserInfoFromLocal} from '../../auth/services/userInfo'
 
 export default React.createClass({
 
@@ -30,10 +32,47 @@ export default React.createClass({
     },
 
     render: function() {
+        const userInfo = getUserInfoFromLocal('userInfo');
+        console.log(userInfo);
         return (
             <div className="home">
                 <Header ref="header"/>
-
+                <ul className="table-view j-table-view">
+                    <li className="table-view-cell">
+                        <Link className="navigate-right">
+                            <div className="user">
+                                <img src="/assets/images/perindex/head.png"/>
+                                <h1>
+                                    <span className="left-des">{userInfo.res.name}{userInfo.res.phone}</span>
+                                    <span className="right-des">个人信息</span>
+                                </h1>
+                            </div>
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="table-view">
+                    <li className="table-view-cell">
+                        <Link className="navigate-right">我的借钱记录</Link>
+                    </li>
+                </ul>
+                <ul className="table-view">
+                    <li className="table-view-cell">
+                        <Link className="navigate-right">账户安全</Link>
+                    </li>
+                </ul>
+                <ul className="table-view">
+                    <li className="table-view-cell">
+                        <Link className="navigate-right">帮助中心</Link>
+                    </li>
+                </ul>
+                <ul className="table-view">
+                    <li className="table-view-cell">
+                        <Link className="navigate-right">关于特华小贷</Link>
+                    </li>
+                </ul>
+                <div className="logout">
+                    <button className="logout-btn" onClick={this.logout}>退出登录</button>
+                </div>
                 <Footer name="home"/>
             </div>
         );
